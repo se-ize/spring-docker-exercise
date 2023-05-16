0660d65ceb721aa9e16aaebb24473f147998f9c2
se-ize <seizenim@gmail.com>
2023-05-16 15:44:02 +0900

    Feat: project build

---
+++ src/main/java/com/example/springdockerexercise/SpringDockerExerciseApplication.java
@@ -0,0 +1,13 @@
+package com.example.springdockerexercise;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDockerExerciseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringDockerExerciseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springdockerexercise/SpringDockerExerciseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springdockerexercise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDockerExerciseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
